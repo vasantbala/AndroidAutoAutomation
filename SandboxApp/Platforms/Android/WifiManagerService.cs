5c68f5cdf268b9c9189c080296daee9c6043d90a
Vasant Balasubramanian <vasant.bala@gmail.com>
2023-12-09 12:33:59 -0600

    Initial Sandbox App to get SSID of active connection

---
+++ SandboxApp/Platforms/Android/WifiManagerService.cs
@@ -0,0 +1,21 @@
+﻿using AndroidNative = Android.App;
+using Android.Net;
+using Android.Net.Wifi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+
+namespace SandboxApp.Services
+{
+    public partial class WifiManagerService
+    {
+        public partial string GetActiveSSID()
+        {
+            WifiManager wifiManager = (WifiManager)AndroidNative.Application.Context.GetSystemService(Service.WifiService);
+            return wifiManager.ConnectionInfo.SSID;
+        }
+    }
+}
---
+++ SandboxApp/Services/WifiManagerService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SandboxApp.Services
+{
+    public partial class WifiManagerService
+    {
+        public partial string GetActiveSSID();
+    }
+}
